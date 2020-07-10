0aed41de79f6dd036fdfde2e6013f76f5360334e
Andre @ code1 <agbowlin@gmail.com>
2020-07-09 22:29:13 -0500

    doc deployment

---
+++ docs/docker-build.sh
@@ -0,0 +1,2 @@
+docker build -t lib-json.liquicode.com:latest .
+docker tag lib-json.liquicode.com:latest 803447307749.dkr.ecr.us-east-1.amazonaws.com/lib-json.liquicode.com
---
+++ docs/docker-deploy.sh
@@ -0,0 +1,1 @@
+docker push 803447307749.dkr.ecr.us-east-1.amazonaws.com/lib-json.liquicode.com
